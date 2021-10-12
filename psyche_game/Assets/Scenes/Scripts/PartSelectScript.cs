6f0aab721cd759231da0047c6bf3a9f4da376844
Jesse Ernst <JEErnst15@gmail.com>
2021-10-12 08:47:46 -0700

    Initial design for selecting objects in strategy phase

---
+++ psyche_game/Assets/Scenes/Scripts/PartSelectScript.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class PartSelectScript : MonoBehaviour
+{
+    public GameObject sprite;
+    public GameObject toRemove;
+    
+    public void OnButtonPress()
+    {
+        // Set object to proper position
+        sprite.transform.position = (Vector2) toRemove.transform.position;
+        // Destroy the gameObject after clicking on it
+        Destroy(toRemove.gameObject);
+    }
+}
