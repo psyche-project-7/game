54d70f235a928185030a1a979f6a52d799f083b1
Kevin Somers <kevin@crowdsentry.io>
2021-10-15 23:57:12 -0400

    Game now accepts input on keyboard for both horizontal and vertical movement

--- psyche_game/Assets/Scenes/Scripts/PlayerHandler.cs
+++ psyche_game/Assets/Scenes/Scripts/PlayerHandler.cs
@@ -14,7 +14,8 @@ public class PlayerHandler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        var movement = Input.GetAxis("Horizontal");
-        transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * MovementSpeed;
+        var movementHorizontal = Input.GetAxis("Horizontal");
+        var movementVertical = Input.GetAxis("Vertical");
+        transform.position += new Vector3(movementHorizontal, movementVertical, 0) * Time.deltaTime * MovementSpeed;
     }
 }
