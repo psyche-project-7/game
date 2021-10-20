7a4fe4c712f71172201e3905f5e7e20ac62a32f2
Jesse Ernst <JEErnst15@gmail.com>
2021-10-19 17:42:59 -0700

    Added scene changing object and test scene. Fully functional

---
+++ psyche_game/Assets/Scripts/SceneSwitch.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitch : MonoBehaviour
+{
+    
+    
+    // Start is called before the first frame update
+    public void OnButtonPress()
+    {
+        SceneManager.LoadScene("Test_Next");
+    }
+
+    
+}
