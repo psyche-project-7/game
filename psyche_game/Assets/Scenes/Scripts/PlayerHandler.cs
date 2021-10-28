f349991b4d62fe0f3ab4085bc73e80d0066bac91
Kevin Somers <kevin@crowdsentry.io>
2021-10-27 23:57:01 -0400

    Added in object for the psyche asteroid and gave it some movement

--- psyche_game/Assets/Scenes/Scripts/PlayerHandler.cs
+++ psyche_game/Assets/Scenes/Scripts/PlayerHandler.cs
@@ -10,18 +10,13 @@ public class PlayerHandler : MonoBehaviour
     public float y = 0;
 
 
-
-
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-
 
 
         x = Mathf.Cos(Time.time)*MovementSpeed;
---
+++ psyche_game/Assets/Scenes/Scripts/PsycheAsteroidHandler.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PsycheAsteroidHandler : MonoBehaviour
+{
+    public float MovementSpeed = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        var movementHorizontal = Input.GetAxis("Horizontal");
+        var movementVertical = Input.GetAxis("Vertical");
+        transform.position += new Vector3(movementHorizontal, movementVertical, 0) * Time.deltaTime * MovementSpeed;
+    }
+}
