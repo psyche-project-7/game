e5e232555872203a0e3576cee0779dde32f55b98
cbarcelon <cbarcelon3@gmail.com>
2021-11-04 20:44:51 -0700

    Enable drag and drop and snap to game objects

---
+++ psyche_game/Assets/Scenes/dragDrop.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dragDrop : MonoBehaviour
+{
+    private Vector3 _dragOffset;
+    private Camera _cam;
+
+    public GameObject[] snapPoints;
+    private float snapSensitivity = 0.8f;
+    private void Awake()
+    {
+        _cam = Camera.main;
+    }
+
+    private void OnMouseDown()
+    {
+        _dragOffset = transform.position - getMousePos();
+    }
+    private void OnMouseDrag()
+    {
+        transform.position = getMousePos() + _dragOffset;
+    }
+
+    private void OnMouseUp()
+    {
+        DropObject();
+    }
+
+    Vector3 getMousePos()
+    {
+        var mousePos = _cam.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0;
+        return mousePos;
+    }
+
+    void DropObject()
+    {
+        for(int i =0; i< snapPoints.Length; i++)
+        {
+            if (Vector3.Distance(snapPoints[i].transform.position, transform.position) < snapSensitivity)
+            {
+                transform.position = new Vector3(snapPoints[i].transform.position.x, snapPoints[i].transform.position.y, snapPoints[i].transform.position.z - 0.1f);
+            }
+        }
+    }
+
+}
