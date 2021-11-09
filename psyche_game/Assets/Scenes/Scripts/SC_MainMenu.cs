31e1f75d2eaad85225d7f14389825a671c7a6b7f
Jeremy Munson <jeremymunson@Jeremys-New-Macbook.local>
2021-11-09 19:59:09 -0300

    2021

--- psyche_game/Assets/Scenes/Scripts/SC_MainMenu.cs
+++ psyche_game/Assets/Scenes/Scripts/SC_MainMenu.cs
@@ -2,8 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//Script for Main Menu
+
 public class NewBehaviourScript : MonoBehaviour
 {
+    AudioSource myAudio;
+
+    
     // Start is called before the first frame update
     void Start()
     {
