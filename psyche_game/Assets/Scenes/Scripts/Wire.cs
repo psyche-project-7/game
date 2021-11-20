27d2fd9d521a8c7508ca603a5f994c5a9f544a93
cbarcelon <cbarcelon3@gmail.com>
2021-11-19 22:48:01 -0800

    add background for wire game

---
+++ psyche_game/Assets/Scenes/Scripts/Wire.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Wire : MonoBehaviour
+{
+    private Image _image;
+
+    private void Awake()
+    {
+        _image = GetComponent<Image>();
+    }
+
+    public void SetColor(Color color)
+    {
+        _image.color = color;
+    }
+}
