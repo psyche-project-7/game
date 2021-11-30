38f32233af47b756ba0503a4705bbff2c9b7b5fe
Kevin Somers <kevin@crowdsentry.io>
2021-11-29 22:51:37 -0500

    Fixed merge conflict

--- psyche_game/Assets/Scripts/Strategy/shipTakeoffMove.cs
+++ psyche_game/Assets/Scripts/Strategy/shipTakeoffMove.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:psyche_game/Assets/Scenes/Scripts/shipTakeoffMove.cs
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -43,50 +42,4 @@ public class shipTakeoffMove : MonoBehaviour
         transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
     }
 }
-=======
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
-public class shipTakeoffMove : MonoBehaviour
-{
-    public GameObject spaceShip;
-    private Vector3 initialPosition;
-    private bool pressed = false;
-
-
-    public Transform target;
-    public float smoothTime = 0.3F;
-    private Vector3 velocity = Vector3.zero;
-
-
-    Vector3 targetPosition;
-
-    void Start()
-    {
-        initialPosition = transform.position;
-        targetPosition = target.TransformPoint(new Vector3(0f, 20.0f, 0f));
-    }
-
-
-    void Update()
-    {
-
-        if (Input.GetKeyDown("space"))
-        {
-            pressed = !pressed;
-
-        }
-        if (pressed)
-        {
-            Invoke("flyUp", 2);
-        }
-        
-    }
-
-    public void flyUp()
-    {        
-        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
-    }
-}
->>>>>>> main:psyche_game/Assets/Scripts/Strategy/shipTakeoffMove.cs
