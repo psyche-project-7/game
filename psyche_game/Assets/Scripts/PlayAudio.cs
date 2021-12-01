7c70096bff6ca6f993d3b7b9c0f040bb9b8110b6
Jeremy Munson <jeremymunson@Jeremys-New-Macbook.local>
2021-11-30 23:35:21 -0300

    renovated main menu

---
+++ psyche_game/Assets/Scripts/PlayAudio.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class PlayAudio : MonoBehaviour
+{
+    public GameObject audioObj;
+
+    public void DropAudio()
+    {
+        Instantiate(audioObj, transform.position, transform.rotation);
+    }
+}
