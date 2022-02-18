9642f9bd0358f4498e3e3fc8f8b090044e810f54
cbarcelon <cbarcelon3@gmail.com>
2022-02-18 14:28:12 -0800

    Create a dialogue system

--- psyche_game/Assets/Scripts/Dialogue/DialogueManager.cs
+++ psyche_game/Assets/Scripts/Dialogue/DialogueManager.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogueManager : MonoBehaviour
 {
+
+    public Text nameText;
+    public Text dialogueText;
+
+    public Animator animator;
     private Queue<string> sentences;
     void Start()
     {
@@ -12,13 +18,37 @@ public class DialogueManager : MonoBehaviour
 
     public void StartDialogue (Dialogue dialogue)
     {
+        animator.SetBool("isOpen", true);
         Debug.Log("Starting conversation with " + dialogue.name);
-
+        nameText.text = dialogue.name;
         sentences.Clear();
 
         foreach (string sentence in dialogue.sentences)
         {
             sentences.Enqueue(sentence);
         }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence ()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+        else
+        {
+            string sentence = sentences.Dequeue();
+            dialogueText.text = sentence;
+            Debug.Log(sentence);
+        }
+    }
+
+    void EndDialogue ()
+    {
+        animator.SetBool("isOpen", false);
+        Debug.Log("End of conversation");
     }
 }
