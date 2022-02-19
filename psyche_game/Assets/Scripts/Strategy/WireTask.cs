a16c97d0d1aa7233c3945517529f8603f38fe80e
cbarcelon <cbarcelon3@gmail.com>
2022-02-18 18:13:31 -0800

    create the shell of a mini game

--- psyche_game/Assets/Scripts/Strategy/WireTask.cs
+++ psyche_game/Assets/Scripts/Strategy/WireTask.cs
@@ -24,7 +24,6 @@ public class WireTask : MonoBehaviour
 
     private void Start()
     {
-        FindObjectOfType<DialogueTrigger>().TriggerDialogue();
         helpStartTime = Time.time;
         _availableColors = new List<Color>(_wireColors);
         _availableLeftWireIndex = new List<int>();
