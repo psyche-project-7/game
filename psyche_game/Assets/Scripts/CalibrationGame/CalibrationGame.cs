3aed3a587380823e7f9df3fdd2c7fbf7ce8ca167
cbarcelon <cbarcelon3@gmail.com>
2022-03-04 15:54:33 -0800

    Mini game improvements

--- psyche_game/Assets/Scripts/CalibrationGame/CalibrationGame.cs
+++ psyche_game/Assets/Scripts/CalibrationGame/CalibrationGame.cs
@@ -1,6 +1,6 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CalibrationGame : MonoBehaviour
 {
@@ -8,46 +8,99 @@ public class CalibrationGame : MonoBehaviour
     private LineRenderer _lineRender;
     // Start is called before the first frame update
     float x_value;
-    int state;
+    int line_state;
+    int wins;
+    bool valid;
+    List<Vector2> points;
+    private BoxCollider2D col;
+    public string nextScene;
+    // -3.8 to -8
+
+    float line_speed;
     void Start()
     {
         _lineRender = GetComponent<LineRenderer>();
-        state = 0;
+        line_state = 0;
         x_value = 0.0f;
+        line_speed = 0.01f;
+        wins = 0;
+        valid = false;
 
     }
+
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            state = 2;
+            if (line_state < 2)
+            {
+                line_state = 2;
+                
+                if (isValid())
+                {
+                    wins += 1;
+                    Debug.Log("Wins " + wins);
+                    if (wins == 3)
+                    {
+                        Invoke("switchScene", 2);
+                    }
+                }
+            }
+            else if (line_state == 2)
+            {
+                line_state = 0;
+                x_value = 0;
+                line_speed += .01f;
+            }
+
         }
 
-        _lineRender.SetPosition(0, new Vector3(x_value, 10, 0));
-        if (state == 0)
+        if (line_state == 0)
         {
             if (x_value < 15)
             {
-                x_value += .01f;
+                x_value += line_speed;
             }
             else
             {
-                state = 1;
+                line_state = 1;
             }
-
+            _lineRender.SetPosition(0, new Vector3(x_value, 10, 0));
+           
+           
         }
-        else if (state == 1)
+        else if (line_state == 1)
         {
             if (x_value > -15)
             {
-                x_value -= .01f;
+                x_value -= line_speed;
             }
             else
             {
-                state = 0;
+                line_state = 0;
             }
+            _lineRender.SetPosition(0, new Vector3(x_value, 10, 0));
+
+        }
+
+    }
+
+    private bool isValid()
+    {
+        if (x_value < -3.8 && x_value > -8) {
+            return true;
+        }
+        else
+        {
+            return false;
         }
     }
+
+    private void switchScene()
+    {
+        SceneManager.LoadScene(nextScene);
+    }
+
 }
